25682474372f2507051d4a2b891217a537552d02
VamsiKrishnaRAVSR <vamsi.krishna@joshsoftware.com>
2024-06-23 23:08:57 +0530

    Added with existing code

--- hangman.go
+++ hangman.go
@@ -26,20 +26,41 @@ func main() {
 		}
 		if userInput == word {
 			fmt.Println("You Win!!")
+			break
+		}
+
+		fmt.Println("\n")
+		fmt.Println(placeholder)  // render the placeholder
+		fmt.Printf("%d", chances) // render the chances left
+		keys := []string{}
+		for k, _ := range entries {
+			keys = append(keys, k)
+		}
+		fmt.Println(keys) // show the letters or words guessed till now.
+		fmt.Printf("Guess a letter or the word: ")
+
+		// take the input
+		str := ""
+		fmt.Scanln(&str)
+		// compare and  update entries placeholder and chances
+		_, ok := entries[str]
+
+		if ok {
+			continue
+		}
+		entries[str] = true
+
+		found := false
+		temp := strings.Split(word, "")
+		for i, v := range temp {
+			if v == str {
+				placeholder[i] = v
+				found = true
+			}
+		}
+		if !found {
+			chances -= 1
 		}
 
 	}
-	fmt.Println("\n")
-	fmt.Println(placeholder)  // render the placeholder
-	fmt.Printf("%d", chances) // render the chances left
-	keys := []string{}
-	for k, _ := range entries {
-		keys = append(keys, k)
-	}
-	fmt.Println(keys) // show the letters or words guessed till now.
-	fmt.Printf("Guess a letter or the word: ")
-
-	// take the input
-	str := ""
-	fmt.Scanln(&str)
 }
